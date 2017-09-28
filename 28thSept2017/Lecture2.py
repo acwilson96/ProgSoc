fa59eeebabee2e45476e293a928d06ae476e71be
yipples <anthonytwyip@gmail.com>
2017-09-28 18:29:56 +0100

    fibonacci

--- 28thSept2017/Lecture2.py
+++ 28thSept2017/Lecture2.py
@@ -15,6 +15,20 @@ def some_fun(firstarg, some_arg=100):
 some_fun(100,300)
 some_fun(100)
 
+
+def add(n1, n2):
+    return n1 + n2
+
+num = add(2,2)
+
 # The fibonacci sequence is as such: 1 1 2 3 5 8 13 21 34
-def fibonacci(prev):
+# f(0)
+# f(n) = f(n-1) + f(n-2)
+def fibonacci(n):
     # Your code here.
+    if (n == 0 or n == 1):
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2) 
+
+
+print(fibonacci(5))
