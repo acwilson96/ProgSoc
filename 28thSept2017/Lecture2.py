edc723b398684ec78eca95765cd79e6c2032bf8f
yipples <anthonytwyip@gmail.com>
2017-09-28 18:10:04 +0100

    AY

---
+++ 28thSept2017/Lecture2.py
@@ -0,0 +1,16 @@
+def add_small (x,y):
+    if x > 10 or y > 10:
+        return 0
+
+        return x + y
+
+        print(add_small(100,10))
+        print(add_small(2,3))
+        
+#task2: create max function - two args, return the bigger, call it my_max
+
+def some_fun(firstarg, some_arg=100):
+    print('I am such a nice function, I got these args:', firstarg, some_arg)
+
+some_fun(100,300)
+some_fun(100)
